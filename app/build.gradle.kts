9328ad0d502be89eb6962926cc2dd8acd33856ce
JuanS-Urbano <sebastianurbano0527@gmail.com>
2026-03-12 20:04:57 -0500

    Correccion 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,14 +12,12 @@ plugins {
 
 android {
     namespace = "com.example.condorapp"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.condorapp"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
